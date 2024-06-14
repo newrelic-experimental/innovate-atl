2dea044a7035fa59a149e677a2f1e750e3aa982e
taylorklemm <taylor.klemm@stablekernel.com>
2024-06-14 12:53:01 -0400

    fix for english

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,21 +1,16 @@
-// pages/api/detect-language.ts
-
 import { NextApiRequest, NextApiResponse } from 'next';
-import {ChatOpenAI} from "@langchain/openai";
-import {HumanMessage, SystemMessage, ToolMessage} from "@langchain/core/messages";
-import {searchWithPrompt} from "@/app/searchLoad";
-import * as sea from "node:sea";
-import { sleep } from 'langchain/util/time';
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { searchWithPrompt } from "@/app/searchLoad";
 
-const systemPromptToEnglish = new SystemMessage(`You are the best translator in the world.  You are going to receive a message in a different language.
-For each message, you will need to detect the language of the message, and translate the phrase into English.  You will return the detected language, three hyphens, and then the translated phrase.
-For examples, if you receive the message 'Hola, ¿cómo estás?', you should detect the language as Spanish and respond with 'Spanish---Hello, how are you?'.  If you receive the message 'Bonjour, comment ça va?', you should detect the language as French and respond with 'French---Hello, how are you?'.  
-If you receive the message 'Hallo, wie geht es dir?', you should detect the language as German and respond with 'German---Hello, how are you?'.  If you receive the message 'Ciao, come stai?', you should detect the language as Italian and respond with 'Italian---Hello, how are you?'.  
+const systemPromptToEnglish = new SystemMessage(`You are the best translator in the world. You are going to receive a message in a different language.
+For each message, you will need to detect the language of the message, and translate the phrase into English. You will return the detected language, three hyphens, and then the translated phrase.
+For examples, if you receive the message 'Hola, ¿cómo estás?', you should detect the language as Spanish and respond with 'Spanish---Hello, how are you?'. If you receive the message 'Bonjour, comment ça va?', you should detect the language as French and respond with 'French---Hello, how are you?'.  
+If you receive the message 'Hallo, wie geht es dir?', you should detect the language as German and respond with 'German---Hello, how are you?'. If you receive the message 'Ciao, come stai?', you should detect the language as Italian and respond with 'Italian---Hello, how are you?'.  
 If you receive the message 'Olá, como você está?', you should detect the language as Portuguese and respond with 'Portuguese---Hello, how are you?'.`);
 
-const systemPromptToLanguage = new SystemMessage(`You are the best translator in the world.  You are going to receive a message in English and a language to translate it into. The messages will be formatted like this: '$message --- $language'. You will return the translated message.
-  For examples, if you receive the message 'Hello, how are you? --- Spanish', you should respond with 'Hola, ¿cómo estás?'.  If you receive the message 'Hello, how are you? --- French', you should respond with 'Bonjour, comment ça va?'.
-`);
+const systemPromptToLanguage = new SystemMessage(`You are the best translator in the world. You are going to receive a message in English and a language to translate it into. The messages will be formatted like this: '$message --- $language'. You will return the translated message.
+For examples, if you receive the message 'Hello, how are you? --- Spanish', you should respond with 'Hola, ¿cómo estás?'. If you receive the message 'Hello, how are you? --- French', you should respond with 'Bonjour, comment ça va?'.`);
 
 type SearchResult = {
   title: string;
@@ -34,7 +29,6 @@ type Data = {
   results?: string;
   error?: string;
 }
-
 
 //Translate phrase into English
 async function Results(phrase: string) {
@@ -76,36 +70,40 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-  if (req.method !== 'POST') {
-    return res.status(405).end(); // Method Not Allowed
-  }
-  let searchResults:  string | undefined;
-  let translatedResult: string | undefined;
-
-  const result = await Results(req.body.text);
-  console.log(result.content);
-
-  if (typeof result.content === "string") {
-    const spl = result.content.split("---");
-    const translation: Translation = {
-      translatedText: spl[1],
-      detectedLanguage: spl[0],
+    if (req.method !== 'POST') {
+      return res.status(405).end(); // Method Not Allowed
     }
-    console.log(translation);
+    let searchResults: string | undefined;
+    let translatedResult: string | undefined;
 
-    //Search with translated phrase to get results
-    searchResults = (await searchWithPrompt(translation.translatedText))?.answer;
-    console.log(searchResults);
+    const result = await Results(req.body.text);
+    console.log(result.content);
 
-    //Translate results back to original language
-    const resultx = (await TranslateToOriginal(searchResults, translation.detectedLanguage))?.content;
-    console.log("translated back - ", resultx);
-    console.log(typeof resultx);
-    if (typeof resultx === "string") {
-      translatedResult = resultx;
+    if (typeof result.content === "string") {
+      const spl = result.content.split("---");
+      const translation: Translation = {
+        translatedText: spl[1],
+        detectedLanguage: spl[0],
+      }
+      console.log(translation);
+
+      //Search with translated phrase to get results
+      searchResults = (await searchWithPrompt(translation.translatedText))?.answer;
+      console.log(searchResults);
+
+      // If the detected language is not English, translate the results back to the original language
+      if (translation.detectedLanguage !== "English") {
+        const resultx = (await TranslateToOriginal(searchResults, translation.detectedLanguage))?.content;
+        console.log("translated back - ", resultx);
+        console.log(typeof resultx);
+        if (typeof resultx === "string") {
+          translatedResult = resultx;
+        }
+      } else {
+        translatedResult = searchResults;
+      }
+      console.log("translatedResult - ", translatedResult);
     }
-    console.log("translatedResult - ", translatedResult);
-  }
 
     if (typeof translatedResult === "string") {
       res.status(200).json({ results: translatedResult });
