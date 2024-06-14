73f321cc70eddea4359288268463d4ed216d71f5
taylorklemm <taylor.klemm@stablekernel.com>
2024-06-14 11:24:50 -0400

    implement actul search

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -3,12 +3,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import {ChatOpenAI} from "@langchain/openai";
 import {HumanMessage, SystemMessage, ToolMessage} from "@langchain/core/messages";
+import {searchWithPrompt} from "@/app/searchLoad";
+import * as sea from "node:sea";
 
 const systemPromptToEnglish = new SystemMessage(`You are the best translator in the world.  You are going to receive a message in a different language.
 For each message, you will need to detect the language of the message, and translate the phrase into English.  You will return the detected language, three hyphens, and then the translated phrase.
 For examples, if you receive the message 'Hola, ¿cómo estás?', you should detect the language as Spanish and respond with 'Spanish---Hello, how are you?'.  If you receive the message 'Bonjour, comment ça va?', you should detect the language as French and respond with 'French---Hello, how are you?'.  
 If you receive the message 'Hallo, wie geht es dir?', you should detect the language as German and respond with 'German---Hello, how are you?'.  If you receive the message 'Ciao, come stai?', you should detect the language as Italian and respond with 'Italian---Hello, how are you?'.  
 If you receive the message 'Olá, como você está?', you should detect the language as Portuguese and respond with 'Portuguese---Hello, how are you?'.`);
+
+
 
 type SearchResult = {
   title: string;
@@ -24,7 +28,7 @@ type Translation = {
 }
 
 type Data = {
-  results?: Translation;
+  results?: string;
   error?: string;
 }
 
@@ -77,24 +81,36 @@ export default async function handler(
   if (req.method !== 'POST') {
     return res.status(405).end(); // Method Not Allowed
   }
+  let searchResults:  string | undefined;
 
   const result = await Results(req.body.text);
   console.log(result.content);
-  //Search with translated phrase to get results
 
-  //Translate results back to original language
+  if (typeof result.content === "string") {
+    const spl = result.content.split("---");
+    const translation: Translation = {
+      translatedText: spl[1],
+      detectedLanguage: spl[0],
+    }
+    console.log(translation);
+
+    //Search with translated phrase to get results
+    searchResults = (await searchWithPrompt(translation.translatedText))?.answer;
+    console.log(searchResults);
+
+    //Translate results back to original language
 
 
 
-    if (typeof result.content === "string") {
-      const spl = result.content.split("---");
-      res.status(200).json({ results: {translatedText: spl[1], detectedLanguage: spl[0]}});
+  }
+    if (typeof searchResults === "string") {
+      res.status(200).json({ results: searchResults });
     } else {
-      res.status(500).json({ error: 'Failed to detect language' });
+      res.status(500).json({ error: 'Failed to translate and search' });
     }
 
   } catch (error) {
     console.error('Error detecting language:', error);
-    res.status(500).json({ error: 'Failed to detect language' });
+    res.status(500).json({ error: 'Failed to translate and search' });
   }
 }
